b634a6b53b391fdf8b28968aa1cac42598c11199
Rebecca Fisher <R.Fisher@aims.gov.au>
2023-04-06 14:05:33 +0800

    copper case study

--- case_study_cu.R
+++ case_study_cu.R
@@ -9,7 +9,11 @@ head(dat)
 jagsfits <- fit.jagsMANEC(data = dat,
                           x.var = "log.x",
                           y.var = "Pcon")
-jagsfits <- modify_jagsMANEC(jagsfits, drop.models = "NECHormesis") 
+
+check.chains(jagsfits,pdf.file="cu_chains.pdf")
+jagsfits <- modify_jagsMANEC(jagsfits, drop.models = c("NECHormesis",
+                                                       "ECxWeibull2", 
+                                                       "ECx4param")) 
 
 plot(jagsfits)
 plot(jagsfits, all_models = TRUE)
@@ -18,6 +22,16 @@ jagsfits$mod.stats |>
                 DIC=round(DIC,3)) |> 
   dplyr::select(DIC, wi) |> 
   write.csv("cs_cu_weights.csv")
+
+
+#NEC function used to derive the no effect concentration where possible. 
+library(drc)
+Cu.nec <- drm(dat$Pcon, dat$Conc, data = dat, fct = NEC.3())
+AIC(Cu.nec)
+plot(Cu.nec, type='all')
+summary(Cu.nec)
+confint(Cu.nec, level = 0.95)
+
 
 i=1
 modnames <- names(jagsfits$mod.fits)
@@ -28,16 +42,16 @@ all_fit_dat <- lapply(jagsfits$mod.fits, FUN = function(x){
 
 head(all_fit_dat)
 
-all_endpoint_dat <- lapply(jagsfits$mod.fits, FUN = function(x){
-  data.frame(
-    EC10=extract_ECx(x, posterior = TRUE)[c(2, 1, 3)],
-    NSEC=extract_NSEC(x, posterior = TRUE)[c(2, 1, 3)],
-    NEC=x$NEC[1:3]    
-  )   
-})|> bind_rows(.id = "Model") |> 
-  data.frame(Model="averaged",
-    EC10=extract_ECx(x)[c(2, 1, 3)],
-    NSEC=extract_NSEC(x)[c(2, 1, 3)],
-    NEC=x$NEC[1:3]   
-  )  
-    
+all_endpoint_dat <- rbind(lapply(jagsfits$mod.fits, FUN = function(x){
+    x$NEC })|> bind_rows(.id = "Model") |> data.frame(), 
+    c(Model="averaged", jagsfits$NEC)) |> 
+  mutate(estimate = if_else(grepl("NEC", Model), "NEC", 
+                            if_else(grepl("ECx", Model), "NSEC",
+                                    "N(S)EC")))
+
+
+save(all_endpoint_dat, all_fit_dat, file = "cu_pred_vals.RData")    
+
+
+
+
